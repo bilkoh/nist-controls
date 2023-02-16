d7d8512252c3b91f7b5b5dea5c711921cb9906d1
bilk0h <bil.koh.sec@gmail.com>
2023-02-15 23:29:56 -0800

    1.4.0: Added cli/npx capability.

--- README.md
+++ README.md
@@ -6,9 +6,26 @@ An npm module to access NIST framework controls.
 
 There are plenty of sources nist for controls: pdfs, websites, excel spread sheets. [(See NIST Data Sources)](#NIST-Data-Sources)
 
-I wanted to plug these controls into various tools or api I create for policy analysis. But the data sources were missing certain fields. Other data sources, were inconsistently labelled, missing baselines, and a host of other problems. So I developed this npm package.
+I wanted to plug these controls into various tools like excel for policy analysis. But the data sources for nist controls were porblematic... missing fields, inconsistent labels, missing baselines, etc. So I developed this npm package.
 
 ## How To Use:
+
+This package can work as cli script or as a library.
+
+### Script
+
+Run this in your console to spit out a csv file for all NIST 800-53 controls:
+
+```
+npx nist-controls --framework SP80053 --format csv --output entire_nist_80053_framework.csv
+```
+
+If you open `entire_nist_80053_framework.csv` in excel it would look something like this:
+![csv-output-screenshot](https://user-images.githubusercontent.com/43228593/219292270-78c9476b-c2c4-4c7f-b8a6-f35ca3dee731.png)
+
+### Library
+
+If you want to use this as a library, check out the examples below:
 
 ```Javascript
 const SP80053 = require("nist-controls").SP80053;
---
+++ cli.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+
+// Options TODO:
+// show-enhancements
+// sortby [either family or by control]
+// exclude-ctrl
+// exclude-family
+
+const yargs = require("yargs");
+
+const options = yargs
+  .usage("Usage: $0 [options]")
+  .option("withdrawn", {
+    describe: "Show withdrawn controls",
+    type: "boolean",
+    default: false,
+  })
+  .option("output", {
+    describe: "Output file",
+    type: "string",
+    demandOption: true,
+  })
+  .option("format", {
+    describe: "Output file format",
+    type: "string",
+    choices: ["CSV"],
+    default: "CSV",
+    coerce: (opt) => opt.toUpperCase(),
+  })
+  .option("verbose", {
+    describe: "Print verbose output",
+    type: "boolean",
+    default: false,
+  })
+  .option("framework", {
+    describe: "Select one framework",
+    type: "string",
+    choices: ["SP80053"],
+    default: "SP80053",
+    coerce: (opt) => opt.toUpperCase(),
+  })
+  .option("debug", {
+    describe: "Enable debug mode",
+    type: "boolean",
+    default: false,
+  })
+  .help().argv;
+
+if (options.debug) {
+  console.dir(options);
+}
+
+const _ = require("lodash");
+const SP80053 = require("./index").SP80053;
+const sp = SP80053();
+
+if (options.framework == "SP80053") {
+  const columns = [
+    "framework",
+    "parent",
+    "controlId",
+    "baselines",
+    "title",
+    "statements",
+    // "enhancements",
+  ];
+  sp.getControls((err, el) => {
+    try {
+      let csvArray = el.map((ctl) => {
+        const renderedCtl = ctl.render();
+        const row = [];
+
+        row.push(renderedCtl.framework);
+        row.push(renderedCtl.parent.title);
+        row.push(renderedCtl.controlId);
+        row.push(renderedCtl.baselines.join(","));
+        row.push(renderedCtl.title);
+        row.push(generateStatementList(renderedCtl.statements).join("\n"));
+
+        return row;
+      });
+
+      csvArray = [...[columns], ...csvArray];
+
+      const output = arrayToCSV(csvArray);
+
+      saveOutput(options.output, output);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+}
+
+const generateStatementList = (arr) => {
+  if (!arr) return arr;
+  return arr.map((e, i) => {
+    const st = e.statement + "\n";
+    let sub = "";
+    if (e.subparts) {
+      sub = generateStatementList(e.subparts);
+      if (_.isArray(sub)) sub = sub.join("");
+    }
+
+    return st + sub;
+  });
+};
+
+const arrayToCSV = (arr) => {
+  const rows = [];
+  for (const row of arr) {
+    const rowStr = row.map((val) => `"${val}"`).join(",");
+    rows.push(rowStr);
+  }
+  return rows.join("\n");
+};
+
+const saveOutput = (filename, output) => {
+  const fs = require("fs");
+
+  fs.writeFile(filename, output, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Output saved to: ", filename);
+  });
+};
